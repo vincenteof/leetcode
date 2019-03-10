d52c49773b3fd4314dac3f8cfef6970bdbf87343
vincenteof <vincenteof@qq.com>
2019-03-10 11:35:12 +0800

    rev no.35

--- src/main/java/SearchInsert/Solution.java
+++ src/main/java/SearchInsert/Solution.java
@@ -5,27 +5,22 @@ public class Solution {
     // no much iteration or recursion, you should first focus on small case and consider how bigger case become small
     public int searchInsert(int[] nums, int target) {
         int i = 0;
-        int j = nums.length;
-        while (i < j) {
-            if (i == j - 1) {
-                if (nums[i] < target) {
-                    return i + 1;
-                } else {
-                    return i;
-                }
-            }
-
-            int mid = (i + j) / 2;
-            int midItem = nums[mid];
-            if (midItem > target) {
-                j = mid;
-            } else if (midItem < target) {
+        int j = nums.length - 1;
+        int mid = i + (j - i) / 2;
+        while (i <= j) {
+            mid = i + (j - i) / 2;
+            if (target > nums[mid]) {
                 i = mid + 1;
+            } else if (target < nums[mid]) {
+                j = mid - 1;
             } else {
                 return mid;
             }
         }
-
-        return j;
+        if (nums[mid] < target) {
+            return j + 1;
+        } else {
+            return i;
+        }
     }
 }
